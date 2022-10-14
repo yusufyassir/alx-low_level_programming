e931db8265c644f414d518819e8721215831ef15
yusufyassir <yusufyassir0@gmail.com>
2022-10-13 22:59:13 -0700

    printf2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
  /**
   * main - Entry point
-  * Description: prints"with proper grammar, but the outcome is a piece of art,"
+  * Description: use ptintf
   * return 0
   */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
