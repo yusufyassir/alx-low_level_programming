a07f30df5ab2c30e175b8cc118cf050a9f96dc60
yusufyassir <yusufyassir0@gmail.com>
2022-10-13 22:52:52 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#iclude <stdio.h>
+ /**
+  * main - Entry point
+  * Description: prints"with proper grammar, but the outcome is a piece of art,"
+  * return 0
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
