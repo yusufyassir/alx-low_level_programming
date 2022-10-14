e15436a4b1c53e8d46b26aa3408235a61f01cc72
yusufyassir <yusufyassir0@gmail.com>
2022-10-14 13:50:59 -0700

    print ni

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ * description: prints numbers from 0 - 9
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
