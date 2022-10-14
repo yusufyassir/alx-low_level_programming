eab87cf55a1077b33b2a42f1dc504072039eb1a2
yusufyassir <yusufyassir0@gmail.com>
2022-10-14 13:18:00 -0700

    alphaa

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description; print all alphabti
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar('ch');
+	}
+	putchar('\n');
+	return (0);
+}
