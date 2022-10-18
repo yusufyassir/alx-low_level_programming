5dfcf8822e3fee0d335f2f1f1b56ed7e5d40e5b8
yusufyassir <yusufyassir0@gmail.com>
2022-10-18 09:01:39 -0700

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check for lower case letters
+ * Return:0
+ */
+int _islower(int c)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
