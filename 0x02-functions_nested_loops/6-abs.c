b31e914166e66bcc3decf615d13c4ae4330d7513
yusufyassir <yusufyassir0@gmail.com>
2022-10-18 12:10:18 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - gets absolute value
+ * Return: num
+ * @n: passed num
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+	return (0);
+	}
+}
