c83be2433c0d0c701bcddb35238e2fbf43670a0e
yusufyassir <yusufyassir0@gmail.com>
2022-10-20 10:23:20 -0700

    task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks if num is a digt
+ * @c: passed num
+ * Return: 1 if digit
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c >= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
