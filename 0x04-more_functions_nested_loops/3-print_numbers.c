1356d7b75c17659e3336383ba86a44d30615367c
yusufyassir <yusufyassir0@gmail.com>
2022-10-20 10:57:32 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints alldigits
+ * Return: o
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for(i = 0; i >10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
