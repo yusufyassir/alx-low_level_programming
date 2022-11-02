c25f3c731864ba7dcd96d5efa090da32d1813842
yusufyassir <yusufyassir0@gmail.com>
2022-11-02 11:53:46 -0700

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - gives length of stirg
+ * @s:string
+ * Return: the length
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len+=  _strlen_recursion(s + 1);
+	}
+	return (len);
+
+}
