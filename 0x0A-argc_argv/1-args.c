1a8c987d9d6f31a2590706b0f99c1d014a51f725
yusufyassir <yusufyassir0@gmail.com>
2022-11-04 18:35:13 -0700

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+/**
+ * main - Prints number of argument passed to the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
