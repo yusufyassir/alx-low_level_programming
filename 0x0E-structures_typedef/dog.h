9a1078699459257cfda48377315d8d26ac28f9d3
yusufyassir <yusufyassir0@gmail.com>
2022-11-14 20:47:37 -0800

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,12 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t: tye of sturct
+ */
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
